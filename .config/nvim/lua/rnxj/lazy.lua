cd43f6cb5a6b2f7cae2360aecb22d1be7757f390
Reuel Nixon <reuelnixon@gmail.com>
2024-05-25 17:56:44 +0530

    chore: disable change detection notification

--- .config/nvim/lua/rnxj/lazy.lua
+++ .config/nvim/lua/rnxj/lazy.lua
@@ -11,4 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("rnxj.plugins")
+require("lazy").setup("rnxj.plugins", {
+  change_detection = {
+    notify = false,
+  },
+})
