b71c2f56d0f8a8f34998028d03368ceb25993a65
Reuel Nixon <reuelnixon@gmail.com>
2024-05-26 23:46:40 +0530

    chore: added session search

--- .config/nvim/lua/rnxj/plugins/auto-sessions.lua
+++ .config/nvim/lua/rnxj/plugins/auto-sessions.lua
@@ -4,12 +4,21 @@ return {
     local auto_session = require 'auto-session'
 
     auto_session.setup {
-      auto_restore_enabled = false,
+      auto_restore_enabled = true,
       auto_session_suppress_dirs = { '~/', '~/Dev/', '~/Downloads', '~/Documents', '~/Desktop/' },
+      session_lens = {
+        buffer_types_to_ignore = {},
+        load_on_setup = true,
+        theme_conf = {
+          border = true,
+        },
+        previewer = false,
+      },
     }
 
     local keymap = vim.keymap
 
+    keymap.set('n', '<leader>wf', require('auto-session.session-lens').search_session, { desc = 'Search sessions', noremap = true }) -- search for session
     keymap.set('n', '<leader>wr', '<cmd>SessionRestore<CR>', { desc = 'Restore session for cwd' }) -- restore last workspace session for current directory
     keymap.set('n', '<leader>ws', '<cmd>SessionSave<CR>', { desc = 'Save session for auto session root dir' }) -- save workspace session for current working directory
   end,
