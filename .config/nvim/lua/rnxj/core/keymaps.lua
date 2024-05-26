142de74d7e9e03c5563c89c66f4b359a19febcfa
Reuel Nixon <reuelnixon@gmail.com>
2024-05-26 14:01:54 +0530

    chore: keymaps to resize window using ctrl + <arrow>

--- .config/nvim/lua/rnxj/core/keymaps.lua
+++ .config/nvim/lua/rnxj/core/keymaps.lua
@@ -30,3 +30,9 @@ keymap.set('n', '<leader>tx', '<cmd>tabclose<CR>', { desc = 'Close current tab' 
 keymap.set('n', '<leader>tn', '<cmd>tabn<CR>', { desc = 'Go to next tab' }) --  go to next tab
 keymap.set('n', '<leader>tp', '<cmd>tabp<CR>', { desc = 'Go to previous tab' }) --  go to previous tab
 keymap.set('n', '<leader>tf', '<cmd>tabnew %<CR>', { desc = 'Open current buffer in new tab' }) --  move current buffer to new tab
+
+-- resize window using <ctrl> arrow keys
+keymap.set('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
+keymap.set('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease Window Height' })
+keymap.set('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease Window Width' })
+keymap.set('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase Window Width' })
